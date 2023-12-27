27f2d2a79b1ae2073667e50fedbc563ee137bdfe
hierach <1060829100@qq.com>
2023-12-27 16:06:23 +0800

    第一次提交

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,19 @@
+package com;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author qin start
+ * @create 2023-12-26-20:28
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(5 & 2);
+
+
+    }
+}
