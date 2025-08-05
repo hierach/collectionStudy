9ee0af5ec38e27e36bc06af493b28fcf7a4f186b
hierach <103297607+hierach@users.noreply.github.com>
2025-08-05 21:14:34 +0800

    Update Main.java

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println(5 & 2);
-        System.out.println(666);
+        System.out.println(777);
 
     }
 }
